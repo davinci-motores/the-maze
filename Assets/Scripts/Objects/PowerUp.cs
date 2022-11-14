c4f239cf026e8e842b63fce47e47560e6fb868c5
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-14 18:57:24 -0300

    GR_Refector-Test-Base
    Se creo los namespaces apuntando a la carpeta Script.
    Se le coloco a cada script su namespace correspondiente.
    Se saco el proyecto de la carpeta The Maze para pasarse dentro del root

---
+++ Assets/Scripts/Objects/PowerUp.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Game.Objects
+{
+    public class PowerUp : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Player
+{
+	public class PlayerController : MonoBehaviour
+	{
+		public void Movement(InputAction.CallbackContext context)
+		{
+			var contexto = context.ReadValue<Vector2>(); //solo se llama cuando el value cambia
+			Debug.Log(contexto);
+		}
+
+		public void Interact(InputAction.CallbackContext context)
+		{
+			if (context.performed) //cuando el context es presionado
+			{
+				Debug.Log("Interactua");
+			}
+		}
+	}	
+}
