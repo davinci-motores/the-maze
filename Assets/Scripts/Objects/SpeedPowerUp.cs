34672f3f61a951292c6c9116898de05a12a32a25
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-17 23:24:37 -0300

    GR_SpeedPowerUp
    Se agrego el metodo activation.

--- Assets/Scripts/Objects/SpeedPowerUp.cs
+++ Assets/Scripts/Objects/SpeedPowerUp.cs
@@ -3,6 +3,10 @@ namespace Game.Objects
 {
     public class SpeedPowerUp : PowerUp
     {
-        
+        protected override void Activation()
+        {
+            Player.DoubleSpeedBy(seconds);
+            //feedback de activacion
+        }
     }
 }
