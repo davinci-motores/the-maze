06b1794e1fb53185ee2dbaf6e99e5e5ca385b7ad
FedeKrug <krug_federico@hotmail.com>
2022-11-18 00:51:38 -0300

    FK_ InvenciblePowerUP
    Agregué el power up de invencibilidad, y funciona (por ahora solo está con Debug.Log)

--- Assets/Scripts/Objects/InvenciblePowerUp.cs
+++ Assets/Scripts/Objects/InvenciblePowerUp.cs
@@ -5,12 +5,19 @@ using Game.Player;
 
 namespace Game.Objects
 {
-	public abstract class InvenciblePowerUp : PowerUp
+	public  class InvenciblePowerUp : PowerUp
 	{
-		
 
+		public override void Activate(PlayerController player)
+		{
+			Debug.Log("Invencible");
+		}
+		public override void Desactivate(PlayerController player)
+		{
+			Debug.Log("Default - Weak");
+		}
 	}
 
-
+	
 }
 
