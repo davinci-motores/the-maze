1f7350fe4ee836320977d60821257df8cdb322e3
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-18 00:45:33 -0300

    GR
    Se modifico el prefab del player para tener el tag Player, tener los collider en la base y que la posicion del mesh este en 0 en x z.
    Se agrego una prueba de power up en la escena Gabriel Development

--- Assets/Scripts/Objects/PowerUp.cs
+++ Assets/Scripts/Objects/PowerUp.cs
@@ -6,8 +6,6 @@ namespace Game.Objects
 {
     public abstract class PowerUp : MonoBehaviour
     {
-		protected GameObject player;
-		protected float seconds;
         public abstract void Activate(PlayerController player);
         public abstract void Desactivate(PlayerController player);
         protected virtual void Destroy()
