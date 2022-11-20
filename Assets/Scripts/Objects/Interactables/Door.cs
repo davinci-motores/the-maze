bcd27476e9c3cbede0cded460510c90eb0cc6a79
FedeKrug <krug_federico@hotmail.com>
2022-11-19 21:28:26 -0300

    FK_Door
    Codigo para la puerta con debug.log.

---
+++ Assets/Scripts/Objects/Interactables/Door.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Game.Objects.Interactables
+{
+
+	public class Door : Interactable
+	{
+		public bool isOpen, hasKey;
+		public void Interact()
+		{
+			if (hasKey)
+			{
+				Debug.Log("I have a key and I'm going to open the door.");
+				Open();
+			}
+			else
+			{
+				Debug.Log("I can't open this door, I need a key to do that.");
+			}
+		}
+		private void Open()
+		{
+			Debug.Log("I opened the door.");
+			isOpen = true;
+		}
+	}
+}
