e6ac866c5e556ddf2cecb01c5e8584a73a312ae8
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-21 20:03:17 -0300

    GR
    Boss Enemy Script

---
+++ Assets/Scripts/Enemies/BoosEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game.Enemies
+{
+    public class BoosEnemy : SeekerEnemy
+    {
+        public override void Attack()
+        {
+            
+        }
+
+        public override void Move()
+        {
+            
+        }
+
+        public override void Chase()
+        {
+            
+        }
+    }
+}
