394a0c93aa94d1446dd5dad87cb7d91a39d9bb21
Matias0193 <109111507+Matias0193@users.noreply.github.com>
2022-11-24 23:18:10 -0300

    EnemyState
    Enemy protected agregado

--- Assets/Scripts/Enemies/States/EnemyState.cs
+++ Assets/Scripts/Enemies/States/EnemyState.cs
@@ -4,6 +4,7 @@ namespace Game.Enemies
 {
     public abstract class EnemyState : MonoBehaviour
     {
+        [SerializeField] protected Enemy enemy;
         public abstract void Enter();
 
         public abstract EnemyState UpdateState();
