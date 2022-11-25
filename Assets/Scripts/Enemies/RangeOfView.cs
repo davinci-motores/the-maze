4038fb5f7801fde080b69828bd8a389d78844693
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2022-11-24 22:45:51 -0300

    FK_ RangeOfView
    Agregue la clase de Range of View al proyecto para los speedy Gomez Enemy

---
+++ Assets/Scripts/Enemies/RangeOfView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeOfView : MonoBehaviour
+{
+    private bool _isTargetInView;
+    [SerializeField] private Transform _target;
+
+    void Awake()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
