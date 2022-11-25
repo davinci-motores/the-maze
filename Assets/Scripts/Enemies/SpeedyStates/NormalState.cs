5fd9a162e75146e547dc1054f99e5ebb11268caf
Matias0193 <109111507+Matias0193@users.noreply.github.com>
2022-11-25 00:35:08 -0300

    NormalStates
    Se agreto el patrol

--- Assets/Scripts/Enemies/SpeedyStates/NormalState.cs
+++ Assets/Scripts/Enemies/SpeedyStates/NormalState.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 namespace Game.Enemies.SpeedyStates
 {
@@ -6,10 +10,18 @@ namespace Game.Enemies.SpeedyStates
     {
         [SerializeField] private RangeOfView _rangeOfView;
         [SerializeField] private AttackState _attackState;
+        [SerializeField] private NavMeshAgent _navMeshAgent;
+
+        [SerializeField] private List<Transform> wayPoint = new List<Transform>();
+        private int wpList;
+        private int _direction;
+
 
         public override void Enter()
         {
-            
+            wpList = 0;
+            _direction = 1;
+            enemy.Move(wayPoint[wpList].position);
         }
 
         public override EnemyState UpdateState()
@@ -19,12 +31,29 @@ namespace Game.Enemies.SpeedyStates
                 return _attackState;
             }
 
-            return null;
+            Patrol();
 
+            return null;
         }
 
         public override void Exit()
         {
+        }
+
+        private void Patrol()
+        {
+            if (_navMeshAgent.remainingDistance <= 2)
+            {
+                wpList += _direction;
+              Debug.Log($"wpList , {wpList}");
+                if (wpList >= wayPoint.Count || wpList <= 0)
+                {
+                    Debug.Log(("a"));
+                    _direction *= -1;
+                }
+
+                enemy.Move(wayPoint[wpList].position);
+            }
         }
     }
 }
