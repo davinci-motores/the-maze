2316bc82482ce2f300323b5fb69dc2230c2f297f
Matias0193 <109111507+Matias0193@users.noreply.github.com>
2022-11-24 22:21:35 -0300

    Update EnemyState
    Update EnemyState

--- Assets/Scripts/Enemies/States/EnemyState.cs
+++ Assets/Scripts/Enemies/States/EnemyState.cs
@@ -4,17 +4,10 @@ namespace Game.Enemies
 {
     public abstract class EnemyState : MonoBehaviour
     {
-        public virtual void Enter()
-        {
-            Debug.Log("Enter");
-        }
-        public virtual void UpdateState()
-        {
-            Debug.Log("UpdateState");
-        }
-        public virtual void Exit()
-        {
-            Debug.Log("Exit");
-        }
+        public abstract void Enter();
+
+        public abstract EnemyState UpdateState();
+
+        public abstract void Exit();
     }
 }
