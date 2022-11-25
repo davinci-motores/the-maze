dc0e96961d34d1fcbd6f90966fd70711e0042bf3
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-11-24 22:16:56 -0300

    GR
    Se dividio el Enemy en Enemy y StateManager

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,25 +6,6 @@ namespace Game.Enemies
     {
         private bool _isAlive = true;
         protected float damage = 1f;
-        protected EnemyState currentState;
-
-        private void Update()
-        {
-            var nextState = currentState.UpdateState();
-
-            if (nextState != null)
-            {
-                ChangeState(nextState);
-            }
-        }
-
-        public void ChangeState(EnemyState nextState)
-        {
-            currentState.Exit();
-            currentState = nextState;
-            currentState.Enter();
-        }
-
         public abstract void Attack();
         public abstract void Move();
     }
---
+++ Assets/Scripts/Enemies/StateManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Enemies
+{
+    public class StateManager : MonoBehaviour
+    {
+        [SerializeField] EnemyState currentState;
+        private void Update()
+        {
+            var nextState = currentState?.UpdateState();
+
+            if (nextState != null)
+            {
+                ChangeState(nextState);
+            }
+        }
+        
+        public void ChangeState(EnemyState nextState)
+        {
+            currentState.Exit();
+            currentState = nextState;
+            currentState.Enter();
+        }
+    }
+}
