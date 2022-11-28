33ee552a2791acef4e012be45b237ed09b00986c
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2022-11-27 21:03:34 -0300

    FK_ Mejora en control de camara
    Se mejoro el control de la camara para usar el cursor bloqueado, y la sensibilidad se manejara con un slider (dentro del inspector)

--- Assets/Scripts/Player/Camera/RotationCameraController.cs
+++ Assets/Scripts/Player/Camera/RotationCameraController.cs
@@ -7,10 +7,14 @@ namespace Game.Player.Camera
     public class RotationCameraController : MonoBehaviour
     {
         [SerializeField] private Transform _followTarget;
-        [SerializeField] private float _rotationXSpeed = 2f;
+        [SerializeField, Range(1,100)] private float _rotationXSpeed = 2f;
         private Vector2 _lookVector;
 
-        public void Look(InputAction.CallbackContext context)
+		private void Awake()
+		{
+            Cursor.lockState = CursorLockMode.Confined;
+		}
+		public void Look(InputAction.CallbackContext context)
         {
             _lookVector = context.ReadValue<Vector2>();
         }
