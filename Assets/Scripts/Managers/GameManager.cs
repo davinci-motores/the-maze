c27e8760e057dfa2d67eac5395a51bc0644fb505
Gabriel Rodriguez <gabriel.drc25.2@gmail.com>
2022-12-02 15:38:00 -0300

    GR
    Se agrego en el GM para que cuando comience el juego le ponga al player la maxima vida

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	[SerializeField] private FloatEventSO _playerHealth;
+	[SerializeField] private FloatEventSO _playerHealthEvent;
+	[SerializeField] private FloatSO _maxPlayerHealth;
+	[SerializeField] private FloatSO _playerHealth;
 	[SerializeField] private GameObject _gameOverScreen;
 	[SerializeField] private GameObject _victoryScreen;
 	[SerializeField] private EventSO _wonEvent;
 
 	private void Awake()
 	{
+		_playerHealth.Value = _maxPlayerHealth.Value;
 		_gameOverScreen.SetActive(false);
 		_victoryScreen.SetActive(false);
 	}
 
 	private void OnEnable()
 	{
-		_playerHealth.RegisterListener(ChangePlayerHealthHandler);
+		_playerHealthEvent.RegisterListener(ChangePlayerHealthHandler);
 		_wonEvent.RegisterListener(WonEventHandler);
 	}
 
 	private void OnDisable()
 	{
-		_playerHealth.RegisterListener(ChangePlayerHealthHandler);
+		_playerHealthEvent.RegisterListener(ChangePlayerHealthHandler);
 		_wonEvent.RegisterListener(WonEventHandler);
 	}
 
